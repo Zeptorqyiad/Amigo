a615cc11d86b3c1b40b08bb698132c59f4323e9f
Max Unanian <Zeptorqyiad99@gmail.com>
2024-11-21 15:42:07 +0500

    Working with responsive page

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // Burger Menu
 
-// const burgerLine = document.querySelector('.burger__list')
-// const burgerMenu = document.querySelector('.icon-menu')
+const burgerLine = document.querySelector('.header--burger__list')
+const burgerMenu = document.querySelector('.menu__icon')
 
-// burgerLine.addEventListener('click', () => {
-//    if (burgerLine) {
-//       burgerLine.classList.toggle('active')
-//       burgerMenu.classList.toggle('active')
-//       document.body.classList.toggle('_lock')
-//    }
-// })
+burgerLine.addEventListener('click', () => {
+   if (burgerLine) {
+      burgerLine.classList.toggle('active')
+      burgerMenu.classList.toggle('active')
+      document.body.classList.toggle('_lock')
+   }
+})
 
-// window.addEventListener('resize', function () {
-//    if (window.matchMedia('(min-width: px)').matches) {
-//       burgerLine.classList.remove('active')
-//       burgerMenu.classList.remove('active')
-//       document.body.classList.remove('_lock')
-//    }
-// })
+window.addEventListener('resize', function () {
+   if (window.matchMedia('(min-width: px)').matches) {
+      burgerLine.classList.remove('active')
+      burgerMenu.classList.remove('active')
+      document.body.classList.remove('_lock')
+   }
+})
 
 // ChangeClick event
 $(function () {
